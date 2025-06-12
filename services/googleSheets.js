4115527e05d15e0d9371e6ec1e20cb9afe068427
hrezolit83 <deduh83@gmail.com>
2025-06-13 02:52:57 +0300

    logs

---
+++ services/googleSheets.js
@@ -0,0 +1,36 @@
+import { google } from 'googleapis';
+import { readFile } from 'fs/promises';
+import { getEnvVar } from '../utils/getEnvVar';
+
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const SHEET_ID = getEnvVar('SHEET_ID');
+
+let sheetsClient;
+
+async function authorize() {
+  if (sheetsClient) return sheetsClient;
+
+  const credentials = JSON.parse(
+    await readFile('google-credentials.json', 'utf-8'),
+  );
+  const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: SCOPES,
+  });
+
+  sheetsClient = google.sheets({ version: 'v4', auth });
+  return sheetsClient;
+}
+
+export async function logToSheet({ date, username, input, mode, response }) {
+  const sheets = await authorize();
+
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: SHEET_ID,
+    range: 'Logs!A1:E1',
+    valueInputOption: 'USER_ENTERED',
+    requestBody: {
+      values: [[date, username, input, mode, response]],
+    },
+  });
+}
